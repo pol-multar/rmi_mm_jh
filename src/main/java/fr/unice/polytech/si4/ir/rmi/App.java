78e3c4e68761535f16329e0e35bed806b1f0885d
Maxime MULTARI <maxime.multari@neuf.fr>
2014-12-01 10:46:43 +0100

    Adding first classes

---
+++ src/main/java/fr/unice/polytech/si4/ir/rmi/App.java
@@ -0,0 +1,13 @@
+package fr.unice.polytech.si4.ir.rmi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
